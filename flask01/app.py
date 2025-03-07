f916444995de6e8382ec5d00805b5057cc09e577
Víctor Manuel Monge Morales <zs22016120@estudiantes.uv.mx>
2025-03-07 10:38:42 -0600

    rutas en flask01

--- flask01/app.py
+++ flask01/app.py
@@ -2,9 +2,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def hola_mundo():
-    return "Hola mundo"
+    return "<h1>Hola</h1><p>Lorem ipsum</p>"
+
+@app.route('/a')
+def fun1():
+    return "<h1>Lorem ipsum</h1><p>Ruta 1</p>"
+
+@app.route('/xml')
+def xml():
+    return '<nombre>Víctor</nombre>'
+
+@app.route('/json')
+def algo():
+    return '{"nombre":"Víctor"}'
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0")
+    # Acepta conexión desde todas las ips
+    app.run(host = "0.0.0.0", debug = True)
